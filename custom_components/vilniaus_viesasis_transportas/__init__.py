f89a7380fb12deafdc68659c33b1e1f0f7f88c17
andiukas <44068098+andiukas@users.noreply.github.com>
2026-02-20 08:49:06 +0200

    Add async setup and unload functions
    Implement async setup and unload for Vilnius Transport.

--- custom_components/vilniaus_viesasis_transportas/__init__.py
+++ custom_components/vilniaus_viesasis_transportas/__init__.py
@@ -1,2 +1,14 @@
+DOMAIN = "vilniaus_transportas"
+PLATFORMS = ["device_tracker"]
+
+async def async_setup_entry(hass, entry):
+    """Set up Vilnius Transport from a config entry."""
+    # Perduodame maršruto informaciją į device_tracker
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass, entry):
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 
